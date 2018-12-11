9d8e188d60f38a7d055b3d82e3739187ee3477b0
root <zen@binary.tree>
2018-12-11 16:38:15 +0800

    1

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
 # Copyright 2018 Tiger. All Rights Reserved.
 # Licensed to MIT under a Contributor Agreement.
 
-__author__ = 'Tiger tiger.huang@gmail.com'
+__author__ = 'Tiger huangzongzhuan@gmail.com'
+
+name = 'pymysql-pool'
