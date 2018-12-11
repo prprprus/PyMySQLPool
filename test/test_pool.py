4b42f078c3d78e6051124b1256164b92280c3606
root <zen@binary.tree>
2018-12-11 17:36:57 +0800

    1

---
+++ test/test_pool.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest.mock import MagicMock
+import uiauto
+
+
+class TestPool(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=1)
