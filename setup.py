ca9a469f04fca2625749f17f9be803e4f4fd9f0b
root <zen@binary.tree>
2018-12-12 17:17:33 +0800

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zongzhenh/pymysql-pool",
-    packages=setuptools.find_packages(exclude=['tests*', ]),
+    packages=setuptools.find_packages(),
     install_requires=[
         'PyMySQL',
     ],
