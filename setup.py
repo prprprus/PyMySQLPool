a2b161fdb5041f8739ec79ce2d87a1d6ed4959b4
root <zen@binary.tree>
2018-12-12 16:40:31 +0800

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyMySQLPool_zongzhenh",
+    name="pymysql-pooling",
     version="0.9.1",
     author="Tiger",
     author_email="huangzongzhuan@gmail.com",
