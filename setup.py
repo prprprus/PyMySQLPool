c52d6348d9968f9be88bec9822864275e47d5b45
root <zen@binary.tree>
2018-12-12 16:16:31 +0800

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyMySQLPool",
+    name="PyMySQLPool_zongzhenh",
     version="0.9.1",
     author="Tiger",
     author_email="huangzongzhuan@gmail.com",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zongzhenh/pymysql-pool",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['tests*', ]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
