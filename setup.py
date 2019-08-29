d1accc8a130cee01d50c743755b518c809866f2a
zongzhenh <huangzongzhuan@gmail.com>
2019-08-29 09:19:00 +0800

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymysql-pooling",
-    version="1.0.3",
+    version="1.0.4",
     description="pymysql-based database connection pool",
     long_description=long_description,
     long_description_content_type="text/markdown",
