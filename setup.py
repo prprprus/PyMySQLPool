92757f18dde76d5c8eebc2cf1056fe1e49dc984a
zongzhenh <huangzongzhuan@gmail.com>
2019-11-08 11:31:28 +0800

    release version 1.0.6 -- support passing any config to connection

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymysql-pooling",
-    version="1.0.5",
+    version="1.0.6",
     description="pymysql-based database connection pool",
     long_description=long_description,
     long_description_content_type="text/markdown",
